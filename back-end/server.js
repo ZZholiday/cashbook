af135636292b3538a0584f32b6fb6033c1c5c99b
张正 <zhangzheng@szkingdom.com>
2020-04-19 00:56:47 +0800

    feat(*):初步完成流程

--- back-end/server.js
+++ back-end/server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
     console.log(__dirname)
-    res.sendFile( __dirname + "/front-end/cashbook.html" );
+    res.sendFile( __dirname + "/cashbook.html" );
  })
+app.use('/public', express.static('public'));
 app.use('/api', getBill);
 app.listen(3000);
 console.log('success listen at port: 3000');
